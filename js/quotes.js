562ef274d48e3229c79df95e7932bad991cc5a3d
Miguel Ángel Moyá <miguel.moya.n@gmail.com>
2017-06-17 13:45:52 +0200

    Update quotes.js

--- js/quotes.js
+++ js/quotes.js
@@ -2,23 +2,23 @@ var url_img = "http://www.trabajarlafelicidad.com/wp-content/uploads/test/";
 var quotes = [
         {
             text: "“eExisten tres posibles respuestas ante un diseño: 'Sí', 'No' y '¡Wow!'. Hay que aspirar a Wow.”",
-			author: "Milton Glaser",
+			author: "5Milton Glaser",
 			img: url_img + "milton_glaser.jpg",
 		},
 		{
-			text: "“eEl diseño es el embajador silencioso de tu marca.”",
+			text: "“5El diseño es el embajador silencioso de tu marca.”",
 			author: "Paul Rand",
-			img: "img/backgrounds/paul_rand.jpg",
+			img: url_img + "paul_rand.jpg",
 		},
 		{
-			text: "“2eUn buen diseño puede soportar la moda de 10 años.”",
+			text: "“5Un buen diseño puede soportar la moda de 10 años.”",
 			author: "Yves Saint Laurent",
-			img: "img/backgrounds/yves_saint_laurent.jpg",
+			img: url_img + "yves_saint_laurent.jpg",
 		},
         {
-			text: "“eSi no es una marca es una mercancía.”",
+			text: "“5Si no es una marca es una mercancía.”",
 			author: "Philip Kotler",
-			img: "img/backgrounds/philip_kotler.jpg",
+			img: url_img + "philip_kotler.jpg",
 		}
     ];
 var quote = quotes[Math.floor(Math.random() * quotes.length)];
