962ecca1eb51654a3233c74487de9aef62aac4a0
Miguel Ángel Moyá Navarro <miguelmoya@iMac-de-Miguel-3.local>
2017-06-17 13:26:55 +0200

    dd

--- js/quotes.js
+++ js/quotes.js
@@ -1,25 +1,26 @@
-var url_img = "https://github.com/miguel-moya/Quotes/blob/master/img/backgrounds",
+var url = "https://github.com/miguel-moya/Quotes/blob/master/img/backgrounds",
 var quotes = [
         {
             text: "“3Existen tres posibles respuestas ante un diseño: 'Sí', 'No' y '¡Wow!'. Hay que aspirar a Wow.”",
 			author: "Milton Glaser",
-			img: url_img + "milton_glaser.jpg",
+			img: url + "milton_glaser.jpg",
 		},
 		{
 			text: "“3El diseño es el embajador silencioso de tu marca.”",
 			author: "Paul Rand",
-			img: url_img + "paul_rand.jpg",
+			img: url + "paul_rand.jpg",
 		},
 		{
 			text: "“3Un buen diseño puede soportar la moda de 10 años.”",
 			author: "Yves Saint Laurent",
-			img: url_img + "yves_saint_laurent.jpg",
+			img: url + "yves_saint_laurent.jpg",
 		},
         {
 			text: "“3Si no es una marca es una mercancía.”",
 			author: "Philip Kotler",
-			img: url_img + "philip_kotler.jpg",
+			img: url + "philip_kotler.jpg",
 		}
     ];
 var quote = quotes[Math.floor(Math.random() * quotes.length)];
 var image = quote.img;
+console.log(image);
